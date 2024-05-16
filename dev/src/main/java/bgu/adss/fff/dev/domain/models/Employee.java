241e9fffd92bc03d28398a10f950889376a0898d
aaroniz <aaroni@post.bgu.ac.il>
2024-05-16 12:44:42 +0300

    Employee entity created

---
+++ dev/src/main/java/bgu/adss/fff/dev/domain/models/Employee.java
@@ -0,0 +1,116 @@
+package bgu.adss.fff.dev.domain.models;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity(name="employee")
+public class Employee {
+    @Id
+    private long id;
+    private String name;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
+    private int bankId;
+    private int bankBranch;
+    private int accountId;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY) // TODO MAP PK
+    private EmploymentTerms terms;
+
+    public Employee(long id, String name, List<Role> roles, EmploymentTerms terms,
+                    int bankId, int bankBranch, int accountId) {
+        this.id = id;
+        this.name = name;
+        this.roles = roles;
+        this.terms = terms;
+
+        this.bankId = bankId;
+        this.bankBranch = bankBranch;
+        this.accountId = accountId;
+    }
+
+    /**
+     * Gets the unique identifier of the employee.
+     *
+     * @return The ID of the employee.
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Gets the name of the employee.
+     *
+     * @return The name of the employee.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name of the employee.
+     *
+     * @param name The new name of the employee.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the list of roles assigned to the employee.
+     *
+     * @return The list of roles of the employee.
+     */
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    /**
+     * Sets the list of roles for the employee.
+     *
+     * @param roles The new list of roles for the employee.
+     */
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    /**
+     * Gets the bank account details of the employee.
+     *
+     * @return The bank account details of the employee.
+     */
+    public String getBank() {
+        return bankId + ":" + bankBranch + ":" + accountId;
+    }
+
+    /**
+     * Sets the bank account details for the employee.
+     *
+     * @param bankId The official state bank id.
+     * @param bankBranch The branch which the employee's account is stored at.
+     * @param accountId The id of the employee's account.
+     */
+    public void setBank(int bankId, int bankBranch, int accountId) {
+        this.bankId = bankId;
+        this.bankBranch = bankBranch;
+        this.accountId = accountId;
+    }
+
+    /**
+     * Gets the employment terms of the employee.
+     *
+     * @return The employment terms of the employee.
+     */
+    public EmploymentTerms getTerms() {
+        return terms;
+    }
+
+    /**
+     * Sets the employment terms for the employee.
+     *
+     * @param terms The new employment terms for the employee.
+     */
+    public void setTerms(EmploymentTerms terms) {
+        this.terms = terms;
+    }
+}
---
+++ dev/src/main/java/bgu/adss/fff/dev/domain/models/EmploymentTerms.java
@@ -0,0 +1,109 @@
+package bgu.adss.fff.dev.domain.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+
+import java.time.LocalDateTime;
+
+@Entity(name="terms")
+public class EmploymentTerms {
+//FIXME    /**
+//     * The employee which is mapped to this terms object.
+//     */
+//    private Employee employee;
+    /**
+     * The date when the employee was employed.
+     */
+    private LocalDateTime startDate;
+    /**
+     * Type of job: full-time/part-time/ contract. Read more at: {@link JobType}.
+     */
+    private JobType jobType;
+    /**
+     * The expected/agreed on monthly salary. May be initiated with -1.
+     */
+    private float monthlySalary;
+    /**
+     * The hourly rate of the employee which was agreed on. May be initiated with -1.<br>
+     * However, {@code hourlyRate == monthlySalary == -1} <b>is not allowed</b>.
+     */
+    private float hourlyRate;
+    /**
+     * The number of days off the employee has.
+     */
+    private int daysOff;
+    /**
+     * The direct manager of the employee.
+     */
+    private Employee manager;
+
+    public EmploymentTerms(LocalDateTime startDate, JobType jobType, Employee manager,
+                           float monthlySalary, float hourlyRate, int daysOff) {
+        if(monthlySalary == hourlyRate && hourlyRate == -1) {
+            throw new IllegalArgumentException("Salary / Rate must be specified!");
+        }
+        this.startDate = startDate;
+        this.jobType = jobType;
+        this.manager = manager;
+
+        this.monthlySalary = monthlySalary;
+        this.hourlyRate = hourlyRate;
+        this.daysOff = daysOff;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public JobType getJobType() {
+        return jobType;
+    }
+
+    public void setJobType(JobType jobType) {
+        this.jobType = jobType;
+    }
+
+    public float getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public void setMonthlySalary(float monthlySalary) {
+        if(monthlySalary == hourlyRate && hourlyRate == -1) {
+            throw new IllegalArgumentException("Salary / Rate must be specified!");
+        }
+
+        this.monthlySalary = monthlySalary;
+    }
+
+    public float getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(float hourlyRate) {
+        if(monthlySalary == hourlyRate && hourlyRate == -1) {
+            throw new IllegalArgumentException("Salary / Rate must be specified!");
+        }
+
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getDaysOff() {
+        return daysOff;
+    }
+
+    public void setDaysOff(int daysOff) {
+        this.daysOff = daysOff;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+}
