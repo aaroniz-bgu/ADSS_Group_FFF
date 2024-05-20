bfd037bbbc0ad807857f9c46302cf143efc8266b
aaroniz <aaroni@post.bgu.ac.il>
2024-05-20 22:26:02 +0300

    Was not implemented correctly.

--- dev/src/main/java/bgu/adss/fff/dev/contracts/FullEmployeeDto.java
+++ dev/src/main/java/bgu/adss/fff/dev/contracts/FullEmployeeDto.java
@@ -1,23 +1,26 @@
 package bgu.adss.fff.dev.contracts;
 
+import bgu.adss.fff.dev.domain.models.JobType;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Contract for the full employee details
- * @param employee Employee details, using {@link EmployeeDto}
- * @param terms Employment terms, using {@link EmployeeTermsDto}
  */
 public record FullEmployeeDto(
-        @JsonProperty("employee") EmployeeDto employee,
-        @JsonProperty("terms") EmployeeTermsDto terms
-) {
-    public FullEmployeeDto {
-        if (employee == null) {
-            throw new IllegalArgumentException("Employee cannot be null");
-        }
-        // TODO Decide if this is necessary
-//        if (terms == null) {
-//            throw new IllegalArgumentException("Terms cannot be null");
-//        }
-    }
-}
+        @JsonProperty("id") Long id,
+        @JsonProperty("name") String name,
+        @JsonProperty("roles") List<RoleDto> roles,
+        @JsonProperty("bankId") int bankId,
+        @JsonProperty("bankBranch") int bankBranch,
+        @JsonProperty("accountId") int accountId,
+        @JsonProperty("startDate") LocalDateTime startDate,
+        @JsonProperty("jobType") JobType jobType,
+        @JsonProperty("monthlySalary") float monthlySalary,
+        @JsonProperty("HourlyRate") float hourlyRate,
+        @JsonProperty("daysOff") int daysOff,
+        @JsonProperty("directManager") EmployeeDto directManager,
+        @JsonProperty("endDate") LocalDateTime endDate
+) { }
