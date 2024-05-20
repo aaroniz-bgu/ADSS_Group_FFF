ced382b3b29e99f285a04539fdae99fdcdefe87e
aaroniz <aaroni@post.bgu.ac.il>
2024-05-20 23:29:01 +0300

    added constants

---
+++ dev/src/main/java/bgu/adss/fff/dev/domain/models/Constants.java
@@ -0,0 +1,8 @@
+package bgu.adss.fff.dev.domain.models;
+
+public class Constants {
+    /**
+     * Used to check if monthly salary / hourly rate are unset.
+     */
+    public static final float NOT_SET = -1;
+}
