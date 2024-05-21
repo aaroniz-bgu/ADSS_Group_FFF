945818f0abdfe33e3392378ad00a7f473cd6f9f2
aaroniz <aaroni@post.bgu.ac.il>
2024-05-21 22:42:09 +0300

    mapping object to help with mapping employee dtos back and forth

--- dev/src/main/java/bgu/adss/fff/dev/controllers/mappers/EmployeeMapper.java
+++ dev/src/main/java/bgu/adss/fff/dev/controllers/mappers/EmployeeMapper.java
@@ -1,14 +1,20 @@
 package bgu.adss.fff.dev.controllers.mappers;
 
 import bgu.adss.fff.dev.contracts.EmployeeDto;
+import bgu.adss.fff.dev.contracts.EmployeeTermsDto;
+import bgu.adss.fff.dev.contracts.FullEmployeeDto;
 import bgu.adss.fff.dev.contracts.RoleDto;
 import bgu.adss.fff.dev.domain.models.Employee;
+import bgu.adss.fff.dev.domain.models.EmploymentTerms;
 import bgu.adss.fff.dev.util.EmployeeUtilHelper;
 
 import java.util.Arrays;
 
 import static bgu.adss.fff.dev.domain.models.Constants.*;
 
+/**
+ * Maps employees to dtos, and the inverse.
+ */
 public class EmployeeMapper {
     public static Employee map(EmployeeDto dto) {
         int[] details = EmployeeUtilHelper.getBankDetailsHelper(dto.bankDetails());
@@ -34,6 +40,73 @@ public class EmployeeMapper {
                         .map(RoleMapper::map)
                         .toArray(RoleDto[]::new),
                 employee.getBank()
+        );
+    }
+
+    public static Employee fullMap(FullEmployeeDto dto) {
+        int[] details = EmployeeUtilHelper.getBankDetailsHelper(dto.bankDetails());
+        int bankId = details[BANK_ID_IND];
+        int bankBranch = details[BANK_BRANCH_IND];
+        int accountId= details[ACCOUNT_ID_IND];
+        EmploymentTerms trm = new EmploymentTerms(
+                dto.startDate(),
+                dto.jobType(),
+                map(dto.directManager()),
+                dto.monthlySalary(),
+                dto.hourlyRate(),
+                dto.daysOff()
+        );
+        return new Employee(
+                dto.id(),
+                dto.name(),
+                Arrays.stream(dto.roles())
+                        .map(RoleMapper::map).toList(),
+                trm,
+                bankId, bankBranch, accountId
+        );
+    }
+
+    public static FullEmployeeDto fullMap(Employee emp) {
+        EmploymentTerms trm = emp.getTerms();
+        return new FullEmployeeDto(
+                emp.getId(),
+                emp.getName(),
+                emp.getRoles()
+                        .stream()
+                        .map(RoleMapper::map)
+                        .toArray(RoleDto[]::new),
+                emp.getBank(),
+                trm.getStartDate(),
+                trm.getJobType(),
+                trm.getMonthlySalary(),
+                trm.getHourlyRate(),
+                trm.getDaysOff(),
+                map(trm.getManager()),
+                trm.getEndDate()
+        );
+    }
+
+    public static EmploymentTerms map(EmployeeTermsDto dto) {
+        return new EmploymentTerms(
+                dto.startDate(),
+                dto.jobType(),
+                map(dto.directManager()),
+                dto.monthlySalary(),
+                dto.hourlyRate(),
+                dto.daysOff()
+        );
+    }
+
+    public static EmployeeTermsDto map(long id, EmploymentTerms trm) {
+        return new EmployeeTermsDto(
+                id,
+                trm.getStartDate(),
+                trm.getJobType(),
+                trm.getMonthlySalary(),
+                trm.getHourlyRate(),
+                trm.getDaysOff(),
+                map(trm.getManager()),
+                trm.getEndDate()
         );
     }
 }
