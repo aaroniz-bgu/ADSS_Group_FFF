0b8b581a1da1a2af2da746a5e482fd9c323346a0
aaroniz <aaroni@post.bgu.ac.il>
2024-05-21 10:26:05 +0300

    imports

--- dev/src/main/java/bgu/adss/fff/dev/domain/models/Employee.java
+++ dev/src/main/java/bgu/adss/fff/dev/domain/models/Employee.java
@@ -1,6 +1,12 @@
 package bgu.adss.fff.dev.domain.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
 import org.springframework.lang.NonNull;
 
 import java.util.List;
