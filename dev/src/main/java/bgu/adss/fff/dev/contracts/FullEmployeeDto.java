f6087d3d52e11df188eb90d50928534321cc11b2
aaroniz <aaroni@post.bgu.ac.il>
2024-05-22 21:15:54 +0300

    imports & datetime -> date, bc we don't need to save time

--- dev/src/main/java/bgu/adss/fff/dev/contracts/FullEmployeeDto.java
+++ dev/src/main/java/bgu/adss/fff/dev/contracts/FullEmployeeDto.java
@@ -1,10 +1,10 @@
 package bgu.adss.fff.dev.contracts;
 
 import bgu.adss.fff.dev.domain.models.JobType;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import java.time.LocalDate;
 
 /**
  * Contract for the full employee details
@@ -14,11 +14,13 @@ public record FullEmployeeDto(
         @JsonProperty("name") String name,
         @JsonProperty("roles") RoleDto[] roles,
         @JsonProperty("bankDetails") String bankDetails,
-        @JsonProperty("startDate") LocalDateTime startDate,
-        @JsonProperty("jobType") JobType jobType,
+        @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+        @JsonProperty("startDate") LocalDate startDate,
+        @JsonProperty("jobType") int jobType,
         @JsonProperty("monthlySalary") float monthlySalary,
         @JsonProperty("HourlyRate") float hourlyRate,
         @JsonProperty("daysOff") int daysOff,
         @JsonProperty("directManager") EmployeeDto directManager,
-        @JsonProperty("endDate") LocalDateTime endDate
+        @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+        @JsonProperty("endDate") LocalDate endDate
 ) { }
