18523e4b2f6c59d840e8788c2f8c752b186370b5
aaroniz <aaroni@post.bgu.ac.il>
2024-05-27 22:59:35 +0300

    added query

--- dev/src/main/java/bgu/adss/fff/dev/data/ShiftRoleRequirementRepository.java
+++ dev/src/main/java/bgu/adss/fff/dev/data/ShiftRoleRequirementRepository.java
@@ -1,8 +1,16 @@
 package bgu.adss.fff.dev.data;
 
+import bgu.adss.fff.dev.domain.models.ShiftDayPart;
 import bgu.adss.fff.dev.domain.models.ShiftRoleRequirement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.DayOfWeek;
+import java.util.List;
+
+@Repository
 public interface ShiftRoleRequirementRepository extends
         JpaRepository<ShiftRoleRequirement, ShiftRoleRequirement.ShiftRoleRequirementId> {
+
+    List<ShiftRoleRequirement> findByIdWeekDayAndIdPart(DayOfWeek weekDay, ShiftDayPart part);
 }
