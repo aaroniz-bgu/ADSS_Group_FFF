48e0cb9c3c7d350bf2b146131354248c70fb158c
aaroniz <aaroni@post.bgu.ac.il>
2024-05-27 22:48:53 +0300

    missing proper annotations

--- dev/src/main/java/bgu/adss/fff/dev/data/EmployeeRepository.java
+++ dev/src/main/java/bgu/adss/fff/dev/data/EmployeeRepository.java
@@ -2,7 +2,9 @@ package bgu.adss.fff.dev.data;
 
 import bgu.adss.fff.dev.domain.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
