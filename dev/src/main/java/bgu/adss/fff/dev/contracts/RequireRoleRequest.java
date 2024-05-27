3055fec7479f286f0c3d98449971d032e92dcce3
aaroniz <aaroni@post.bgu.ac.il>
2024-05-27 21:49:45 +0300

    match model

--- dev/src/main/java/bgu/adss/fff/dev/contracts/RequireRoleRequest.java
+++ dev/src/main/java/bgu/adss/fff/dev/contracts/RequireRoleRequest.java
@@ -2,21 +2,19 @@ package bgu.adss.fff.dev.contracts;
 
 import bgu.adss.fff.dev.domain.models.ShiftDayPart;
 import com.fasterxml.jackson.annotation.JsonProperty;
-// TODO import the ShiftDayPart enum
 
-import java.time.LocalDateTime;
-import java.util.Collection;
+import java.time.LocalDate;
 
 /**
  * Contract for requiring a role for a specific shift
  * Used for both adding and removing roles
  * @param date The date of the shift
- * @param shift According to the {@link ShiftDayPart} enum
+ * @param shift According to the {@link ShiftDayPart} enum ordinal
  * @param role The roles to add or remove
  * <!--@param addOrRemove True for add, false for remove-->
  */
 public record RequireRoleRequest(
-        @JsonProperty("date") LocalDateTime date,
-        @JsonProperty("shift") ShiftDayPart shift,
+        @JsonProperty("date") LocalDate date,
+        @JsonProperty("shift") int shift,
         @JsonProperty("roles") String role
         ) { }
