20a3e78f656c7623ec72c8ae068c213f410f8dcb
aaroniz <aaroni@post.bgu.ac.il>
2024-05-28 21:14:28 +0300

    added query for range of shifts

--- dev/src/main/java/bgu/adss/fff/dev/data/ShiftRepository.java
+++ dev/src/main/java/bgu/adss/fff/dev/data/ShiftRepository.java
@@ -3,6 +3,16 @@ package bgu.adss.fff.dev.data;
 import bgu.adss.fff.dev.domain.models.EmbeddedShiftId;
 import bgu.adss.fff.dev.domain.models.Shift;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ShiftRepository extends JpaRepository<Shift, EmbeddedShiftId> {
+
+    @Query("select s from shifts s where s.id.date between :fromDate and :toDate")
+    List<Shift> getRangeOfShifts(
+            @Param("fromDate") LocalDate fromDate,
+            @Param("toDate") LocalDate toDate);
 }
