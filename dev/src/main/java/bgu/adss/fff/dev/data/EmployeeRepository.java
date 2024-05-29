3f77992512a62e99caeb61733c31aecc7178d1d3
sachsd <sachsd@post.bgu.ac.il>
2024-05-29 17:56:18 +0300

    Added find EmployeeByBranch query in EmployeeRepository

--- dev/src/main/java/bgu/adss/fff/dev/data/EmployeeRepository.java
+++ dev/src/main/java/bgu/adss/fff/dev/data/EmployeeRepository.java
@@ -1,10 +1,17 @@
 package bgu.adss.fff.dev.data;
 
 import bgu.adss.fff.dev.domain.models.Employee;
+import bgu.adss.fff.dev.domain.models.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+    @Query("select e from employee e where e.branch = :branch")
+    List<Employee> findEmployeesByBranch(@Param("branch") Branch branch);
 }
