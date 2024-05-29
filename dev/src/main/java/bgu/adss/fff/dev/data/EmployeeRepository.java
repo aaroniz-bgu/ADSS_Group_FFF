80975615467229f7e8964551ffcec196d2a86da9
sachsd <sachsd@post.bgu.ac.il>
2024-05-29 18:41:18 +0300

    Removed unused imports.

--- dev/src/main/java/bgu/adss/fff/dev/data/EmployeeRepository.java
+++ dev/src/main/java/bgu/adss/fff/dev/data/EmployeeRepository.java
@@ -4,8 +4,6 @@ import bgu.adss.fff.dev.domain.models.Employee;
 import bgu.adss.fff.dev.domain.models.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
