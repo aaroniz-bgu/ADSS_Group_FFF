62834e108a54d9f17ed608b4cbe69a7af3b83818
aaroniz <aaroni@post.bgu.ac.il>
2024-05-31 18:18:03 +0300

    now using `AppException` and passing HttpStatus

--- dev/src/main/java/bgu/adss/fff/dev/exceptions/RoleException.java
+++ dev/src/main/java/bgu/adss/fff/dev/exceptions/RoleException.java
@@ -1,14 +1,16 @@
 package bgu.adss.fff.dev.exceptions;
 
-public class RoleException extends RuntimeException{
-    private RoleException(String msg) { super(msg); }
+import org.springframework.http.HttpStatus;
+
+public class RoleException extends AppException {
+    private RoleException(String msg, HttpStatus status) { super(msg, status); }
 
     public static RoleException alreadyExists(String name) {
-        return new RoleException("Role with name " + name + " already exists.");
+        return new RoleException("Role with name " + name + " already exists.", HttpStatus.CONFLICT);
     }
 
     public static RoleException notFound(String name) {
-        return new RoleException("Role with name " + name + " was not found.");
+        return new RoleException("Role with name " + name + " was not found.", HttpStatus.NOT_FOUND);
     }
 
     public static RoleException illegalField(String name, String resource, String details) {
@@ -17,6 +19,6 @@ public class RoleException extends RuntimeException{
                 Resource: %s
                 Details:
                 %s
-                """, name, resource, details));
+                """, name, resource, details), HttpStatus.BAD_REQUEST);
     }
 }
