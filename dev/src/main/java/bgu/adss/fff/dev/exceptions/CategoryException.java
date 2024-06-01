04cea28033d96f5485bf65840119f88a7a4afb1a
ShalevKk <kayatsha@post.bgu.ac.il>
2024-06-01 13:53:12 +0300

    Implemented one CategoryException and some CategoryService functions

--- dev/src/main/java/bgu/adss/fff/dev/exceptions/CategoryException.java
+++ dev/src/main/java/bgu/adss/fff/dev/exceptions/CategoryException.java
@@ -2,4 +2,8 @@ package bgu.adss.fff.dev.exceptions;
 
 public class CategoryException extends RuntimeException{
 
+    public CategoryException(String message){
+        super(message);
+    }
+
 }
--- dev/src/main/java/bgu/adss/fff/dev/services/CategoryServiceImpl.java
+++ dev/src/main/java/bgu/adss/fff/dev/services/CategoryServiceImpl.java
@@ -2,6 +2,7 @@ package bgu.adss.fff.dev.services;
 
 import bgu.adss.fff.dev.data.CategoryRepository;
 import bgu.adss.fff.dev.domain.models.Category;
+import bgu.adss.fff.dev.domain.models.Product;
 import bgu.adss.fff.dev.exceptions.CategoryException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,28 +22,62 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public Category createCategory(Category category) {
-        // TODO: Check if category already exists
+
+        if(category == null){
+            throw new CategoryException("Category cannot be null");
+        }
+
+        if (repository.existsById(category.getCategoryName())) {
+            throw new CategoryException("Category already exists");
+        }
+
         // TODO: Any other business logic
+
         return repository.save(category);
     }
 
     @Override
     public Category getCategory(String name) {
-        return repository.findById(name).orElseThrow(CategoryException::new);
+
+        if(name == null) {
+            throw new CategoryException("Category name cannot be null");
+        }
+
+        return repository.findById(name).orElseThrow(() -> new CategoryException("Category not found"));
     }
 
     @Override
     public List<Category> getCategory() {
-        return null;
+
+        return repository.findAll();
+
     }
 
     @Override
     public Category updateCategory(String name, Category category) {
-        return null;
+
+        if (category == null) {
+            throw new CategoryException("Product cannot be null");
+        }
+
+        if (!repository.existsById(category.getCategoryName())) {
+            throw new CategoryException("Product not found");
+        }
+
+        //TODO: Any other business logic
+
+        return repository.save(category);
+
     }
 
     @Override
     public void deleteCategory(String name) {
+
+        if(!repository.existsById(name)){
+            throw new CategoryException("Category not found");
+        }
+
+        repository.deleteById(name);
 
     }
 
