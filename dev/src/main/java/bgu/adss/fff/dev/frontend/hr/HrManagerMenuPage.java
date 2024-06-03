3062d5b14c70f574bdd95ae7e55b0f7f3199c9e8
aaroniz <aaroni@post.bgu.ac.il>
2024-06-03 23:39:55 +0300

    created menu for hr

---
+++ dev/src/main/java/bgu/adss/fff/dev/frontend/hr/HrManagerMenuPage.java
@@ -0,0 +1,65 @@
+package bgu.adss.fff.dev.frontend.hr;
+
+import bgu.adss.fff.dev.contracts.EmployeeDto;
+import bgu.adss.fff.dev.frontend.cli.components.InputComponent;
+import bgu.adss.fff.dev.frontend.cli.components.ListComponent;
+import bgu.adss.fff.dev.frontend.cli.components.StateEvent;
+import bgu.adss.fff.dev.frontend.cli.uikit.AbstractUserComponent;
+
+import java.io.PrintStream;
+import java.util.List;
+
+public class HrManagerMenuPage extends AbstractUserComponent {
+
+    private final EmployeeDto employee;
+
+    private InputComponent choiceInput;
+
+    protected HrManagerMenuPage(PrintStream out, EmployeeDto employee) {
+        super(out);
+
+        this.employee = employee;
+
+        this.choiceInput = new InputComponent("Choose operation to perform:");
+
+        page.add(new LogoComponent("Welcome " + employee.name() + " : HUMAN RESOURCES DEPARTMENT"));
+        page.add(new ListComponent<String>(List.of(
+                "Register new employee",
+                "Edit existing employee details",
+                "[#] Shift Assignment Interface", // Choose week->show shifts table??
+                "View shift history",
+                "Require roles",
+                "Exit"
+        )));
+        choiceInput.subscribe(this::onChoice);
+        page.add(choiceInput);
+    }
+
+    private void onChoice(StateEvent event) {
+        boolean rerender = true;
+        try {
+            int choice = Integer.parseInt(event.getData());
+            switch (choice) {
+                case 1: //TODO
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                default:
+                    rerender = false;
+                    break;
+            }
+            if(rerender) this.render();
+        } catch(NumberFormatException e) {
+            out.println("Please provide your choice as a number without any additional characters.");
+            this.render();
+        }
+    }
+}
