24e0ddbecb0967d2e593e147e2cf5b317dfa6383
aaroniz <aaroni@post.bgu.ac.il>
2024-06-03 22:47:04 +0300

    calling front end thread

--- dev/src/main/java/bgu/adss/fff/dev/DevApplication.java
+++ dev/src/main/java/bgu/adss/fff/dev/DevApplication.java
@@ -1,5 +1,6 @@
 package bgu.adss.fff.dev;
 
+import bgu.adss.fff.dev.frontend.FrontendApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,9 @@ public class DevApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DevApplication.class, args);
+        new Thread(() -> {
+            FrontendApp app = new FrontendApp();
+        }).start();
     }
 
 }
