2aa6e4ed7ad3cf7795478acc3ef033c9d3d034aa
aaroniz <aaroni@post.bgu.ac.il>
2024-06-03 22:45:50 +0300

    front end app to init front

---
+++ dev/src/main/java/bgu/adss/fff/dev/frontend/FrontendApp.java
@@ -0,0 +1,17 @@
+package bgu.adss.fff.dev.frontend;
+
+import bgu.adss.fff.dev.frontend.cli.uikit.AbstractUserComponent;
+import bgu.adss.fff.dev.frontend.cli.uikit.TerminalApp;
+import bgu.adss.fff.dev.frontend.hr.LoginPage;
+
+public class FrontendApp extends TerminalApp {
+    public static final String URI_PATH = "http://localhost:8080";
+
+    public FrontendApp() {
+        AbstractUserComponent page = new LoginPage(System.out);
+        while(!Thread.currentThread().isInterrupted()) {
+            page.render();
+        }
+        this.close();
+    }
+}
