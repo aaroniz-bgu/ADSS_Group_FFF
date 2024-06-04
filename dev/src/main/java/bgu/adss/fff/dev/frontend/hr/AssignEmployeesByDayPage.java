d46c43361949fdc1642d0322fc07e840e091588d
aaroniz <aaroni@post.bgu.ac.il>
2024-06-04 21:04:55 +0300

    (𖦹﹏𖦹;)

---
+++ dev/src/main/java/bgu/adss/fff/dev/frontend/hr/AssignEmployeesByDayPage.java
@@ -0,0 +1,114 @@
+package bgu.adss.fff.dev.frontend.hr;
+
+import bgu.adss.fff.dev.contracts.EmployeeDto;
+import bgu.adss.fff.dev.contracts.ErrorDetails;
+import bgu.adss.fff.dev.contracts.ShiftDto;
+import bgu.adss.fff.dev.frontend.cli.components.InputComponent;
+import bgu.adss.fff.dev.frontend.cli.components.StateEvent;
+import bgu.adss.fff.dev.frontend.cli.components.TableComponent;
+import bgu.adss.fff.dev.frontend.cli.uikit.AbstractUserComponent;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.List;
+
+import static bgu.adss.fff.dev.frontend.FrontendApp.URI_PATH;
+
+public class AssignEmployeesByDayPage extends AbstractUserComponent {
+
+    private static final String ROUTE = URI_PATH + "/shift/assign";
+
+    private List<String> HEADER = List.of("Employee Name", "Employee ID", "Morning/Evening");
+
+    private final RestTemplate template;
+
+    private final InputComponent daypartInput;
+    private final InputComponent employeeInput;
+
+    private ShiftDto shiftA;
+    private ShiftDto shiftB;
+
+    private ShiftDto request;
+
+    public AssignEmployeesByDayPage(PrintStream out, ShiftDto shiftA, ShiftDto shiftB) {
+        super(out);
+
+        this.shiftA = shiftA;
+        this.shiftB = shiftB;
+
+        List<List<String>> rows = new LinkedList<>();
+        listHelper(rows, shiftA);
+        listHelper(rows, shiftB);
+
+        template = new RestTemplate();
+
+        daypartInput = new InputComponent("Morning/Evening shift? [0/1]:");
+        employeeInput = new InputComponent("Insert the employee ids separated by comma(,):");
+
+        daypartInput.subscribe(this::onDayPartInput);
+        employeeInput.subscribe(this::onEmployeeInput);
+
+        page.add(new LogoComponent("Assign Employees To Shift"));
+        page.add(new TableComponent<>(HEADER, rows, 150, 24));
+        page.add(daypartInput);
+        page.add(employeeInput);
+    }
+
+    private void onDayPartInput(StateEvent event) {
+        try {
+            int part = Integer.parseInt(event.getData());
+            request = part == 0 ? shiftA : part == 1 ? shiftB : null;
+            if(request == null) throw new RuntimeException();
+        } catch (RuntimeException e) {
+            out.println("Please respond only with the available options!");
+            daypartInput.render(out);
+        }
+    }
+
+    private void onEmployeeInput(StateEvent event) {
+        String[] ids = event.getData().replace(" ","").split(",");
+        try {
+            if (ids.length == 0) throw new IllegalArgumentException();
+            EmployeeDto[] assigned = new EmployeeDto[ids.length];
+            for(int i = 0; i < assigned.length; i++) {
+                assigned[i] = inefficientGetEmployeeById(
+                        request.availableEmployees(), Long.parseUnsignedLong(ids[i])
+                );
+                if(assigned[i] == null) throw new IllegalArgumentException();
+            }
+            request = new ShiftDto(request.date(), request.shift(), request.branch(), request.isLocked(),
+                    request.availableEmployees(), assigned, request.requiredRoles());
+
+
+        } catch (IllegalArgumentException e) {
+            out.println("Please make sure your input is correctly separated by comma and contains only ids that are available to work that shift!");
+            employeeInput.render(out);
+        }
+    }
+
+    private void sendRequest() {
+        try {
+            template.put(ROUTE, request);
+        } catch (RestClientResponseException e) {
+            ErrorDetails err = e.getResponseBodyAs(ErrorDetails.class);
+            out.println(err.message());
+        } catch (Exception e) {
+            out.println("an unexpected error occurred please try again later.");
+        }
+    }
+
+    private EmployeeDto inefficientGetEmployeeById(EmployeeDto[] arr, long id) {
+        for(EmployeeDto emp : arr) {
+            if(emp.id() == id) return emp;
+        }
+        return null;
+    }
+
+    private void listHelper(List<List<String>> rows, ShiftDto shift) {
+        for(EmployeeDto emp : shift.availableEmployees()) {
+            rows.add(List.of(emp.name(), emp.id()+"", shift.shift() == 0 ? "M" : "E"));
+        }
+    }
+}
