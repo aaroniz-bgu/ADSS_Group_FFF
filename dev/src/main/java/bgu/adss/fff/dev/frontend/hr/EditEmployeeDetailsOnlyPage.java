c25125bbfe5c6a19bf6f171ce5a7ffc67dd6a0e1
aaroniz <aaroni@post.bgu.ac.il>
2024-06-05 00:29:52 +0300

    pffffft

---
+++ dev/src/main/java/bgu/adss/fff/dev/frontend/hr/EditEmployeeDetailsOnlyPage.java
@@ -0,0 +1,134 @@
+package bgu.adss.fff.dev.frontend.hr;
+
+import bgu.adss.fff.dev.contracts.EmployeeDto;
+import bgu.adss.fff.dev.contracts.ErrorDetails;
+import bgu.adss.fff.dev.contracts.RoleDto;
+import bgu.adss.fff.dev.frontend.cli.components.InputComponent;
+import bgu.adss.fff.dev.frontend.cli.components.LabelComponent;
+import bgu.adss.fff.dev.frontend.cli.components.StateEvent;
+import bgu.adss.fff.dev.frontend.cli.uikit.AbstractUserComponent;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.PrintStream;
+import java.util.Arrays;
+
+import static bgu.adss.fff.dev.frontend.FrontendApp.URI_PATH;
+
+public class EditEmployeeDetailsOnlyPage extends AbstractUserComponent {
+
+    private static final String ROLES_ROUTE = URI_PATH + "/role";
+    private static final String BRANCH_ROUTE = URI_PATH + "/branch";
+    private static final String EMPLOYEE_ROUTE = URI_PATH + "/employee/";
+
+    private final RestTemplate template;
+
+    private final InputComponent nameInput;
+    private final InputComponent rolesInput;
+    private final InputComponent bankInput;
+    private final InputComponent branchInput;
+
+    private String rolesList, branchList;
+
+    private long id;
+    private String name;
+    private RoleDto[] roles;
+    private String bankDetails;
+    private String branch;
+    public EditEmployeeDetailsOnlyPage(PrintStream out, long id) {
+        super(out);
+
+        this.template = new RestTemplate();
+        try {
+            RoleDto[] roles = template.getForEntity(ROLES_ROUTE, RoleDto[].class).getBody();
+            rolesList = Arrays.toString(roles);
+            String[] branches = template.getForEntity(BRANCH_ROUTE, String[].class).getBody();
+            branchList = Arrays.toString(branches);
+
+            EmployeeDto emp = template.getForEntity(EMPLOYEE_ROUTE+id, EmployeeDto.class).getBody();
+            name = emp.name();
+            this.roles = emp.roles();
+            bankDetails = emp.bankDetails();
+            branch = emp.branchName();
+        } catch (Exception e) {
+            //log?
+        }
+
+        this.id = id;
+
+        this.nameInput = new InputComponent("Insert the name of the employee:");
+        this.rolesInput = new InputComponent("Insert the roles this employee has:\n" +
+                "Pick from the list (or leave empty), to separate use comma (,): "+rolesList);
+        this.bankInput = new InputComponent("Insert the bank details of the employee"+
+                "\n Using the following format: [BANK ID]:[BANK BRANCH]:[ACCOUNT ID] !");
+        this.branchInput = new InputComponent("Insert the branch this employee is working at: \n" +
+                "Available branches: "+branchList);
+
+        nameInput.subscribe(this::onNameInput);
+        rolesInput.subscribe(this::onRolesInput);
+        bankInput.subscribe(this::onBankInput);
+        branchInput.subscribe(this::onBranchInput);
+
+        page.add(new LogoComponent("Edit Existing Employee"));
+        page.add(new LabelComponent("Leave empty [ENTER] if you don't want to change the field!"));
+        page.add(nameInput);
+        page.add(rolesInput);
+        page.add(bankInput);
+        page.add(branchInput);
+    }
+
+    private void onNameInput(StateEvent event) {
+        if(event.getData() == null) {
+            out.println("Input error occurred.");
+            nameInput.render(out);
+        } else if(event.getData().isBlank()) {
+            // do not change name.
+        } else {
+            this.name = event.getData();
+        }
+    }
+
+    private void onRolesInput(StateEvent event) {
+        if(!event.getData().isBlank()) {
+            String[] roleTitles = event.getData().replace(" ","").split(",");
+            roles = new RoleDto[roleTitles.length];
+            for(int i = 0 ; i< roles.length; i++) {
+                roles[i] = new RoleDto(roleTitles[i], false);
+            }
+        } // else we're not updating the roles.
+    }
+
+    private void onBankInput(StateEvent event) {
+        if(!event.getData().isBlank()) return; //no need to worry...
+
+        char[] input = event.getData().toCharArray();
+        int count = 0;
+        for(char c : input) {
+            if(c == ':') count++;
+        }
+        if(count != 2) {
+            out.println("please supply bank details as formatted!");
+            bankInput.render(out);
+            return;
+        }
+        bankDetails = event.getData();
+    }
+
+    private void onBranchInput(StateEvent event) {
+        if(!event.getData().isBlank()) this.branch = event.getData();
+        sendRequest();
+    }
+
+    private  void sendRequest() {
+        try {
+            template.put(EMPLOYEE_ROUTE + id, new EmployeeDto(
+                    id, name, roles, bankDetails, branch
+            ));
+        } catch (RestClientResponseException e) {
+            ErrorDetails err = e.getResponseBodyAs(ErrorDetails.class);
+            out.println(err.message());
+        } catch (Exception e) {
+            out.println("an unexpected error occurred please try again later.");
+        }
+    }
+}
--- dev/src/main/java/bgu/adss/fff/dev/frontend/hr/EditEmployeeDetailsPage.java
+++ dev/src/main/java/bgu/adss/fff/dev/frontend/hr/EditEmployeeDetailsPage.java
@@ -1,11 +1,49 @@
 package bgu.adss.fff.dev.frontend.hr;
 
+import bgu.adss.fff.dev.frontend.cli.components.InputComponent;
+import bgu.adss.fff.dev.frontend.cli.components.StateEvent;
 import bgu.adss.fff.dev.frontend.cli.uikit.AbstractUserComponent;
 
 import java.io.PrintStream;
 
 public class EditEmployeeDetailsPage extends AbstractUserComponent {
+
+    private final InputComponent idInput;
+    private final InputComponent termsOrEmployeeInput;
+
+    private long id;
     public EditEmployeeDetailsPage(PrintStream out) {
         super(out);
+
+        idInput = new InputComponent("Insert employee ID:");
+        termsOrEmployeeInput = new InputComponent("Would you like to edit employee details[E] or the employment terms[T]?");
+
+        idInput.subscribe(this::onIdInput);
+        termsOrEmployeeInput.subscribe(this::onTermsOrEmployeeInput);
+
+        page.add(new LogoComponent("Edit Employee Details"));
+        page.add(idInput);
+        page.add(termsOrEmployeeInput);
+    }
+
+    private void onIdInput(StateEvent event) {
+        try {
+            this.id = Long.parseUnsignedLong(event.getData());
+        } catch (NumberFormatException e) {
+            out.println("Please type a proper id which is supposed to be a number.");
+            idInput.render(out);
+        }
+    }
+
+    private void onTermsOrEmployeeInput(StateEvent event) {
+        String choice = event.getData().toLowerCase();
+        if(choice.equals("t")) {
+
+        } else if (choice.equals("e")) {
+            new EditEmployeeDetailsOnlyPage(out, id).render();
+        } else {
+            out.println("Please insert only values that you were instructed to!");
+            termsOrEmployeeInput.render(out);
+        }
     }
 }
