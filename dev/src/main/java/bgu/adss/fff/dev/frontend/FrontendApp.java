5ca5eebf3ec07aaa05a5fa60917201797b65d5a7
Noam Argaman <noamarg@post.bgu.ac.il>
2024-06-06 20:37:22 +0300

    Created pages for reports and connected them

--- dev/src/main/java/bgu/adss/fff/dev/frontend/FrontendApp.java
+++ dev/src/main/java/bgu/adss/fff/dev/frontend/FrontendApp.java
@@ -2,14 +2,14 @@ package bgu.adss.fff.dev.frontend;
 
 import bgu.adss.fff.dev.frontend.cli.uikit.AbstractUserComponent;
 import bgu.adss.fff.dev.frontend.cli.uikit.TerminalApp;
-import bgu.adss.fff.dev.frontend.inventory.AddProductPage;
+import bgu.adss.fff.dev.frontend.inventory.InventoryMenuPage;
 
 public class FrontendApp extends TerminalApp {
     public static final String URI_PATH = "http://localhost:8080";
 
     public FrontendApp() {
         before();
-        AbstractUserComponent page = new AddProductPage(System.out);
+        AbstractUserComponent page = new InventoryMenuPage(System.out);
         while(!Thread.currentThread().isInterrupted()) {
             page.render();
         }
