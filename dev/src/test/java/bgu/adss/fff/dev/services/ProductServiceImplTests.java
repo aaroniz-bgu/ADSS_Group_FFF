49bd6f1af14e958a2abc8166981ca5240ad3640f
Noam Argaman <noamarg@post.bgu.ac.il>
2024-06-10 21:52:37 +0300

    created first 2 tests

--- dev/src/test/java/bgu/adss/fff/dev/services/ProductServiceImplTests.java
+++ dev/src/test/java/bgu/adss/fff/dev/services/ProductServiceImplTests.java
@@ -1,11 +1,20 @@
 package bgu.adss.fff.dev.services;
 
 import bgu.adss.fff.dev.data.ProductRepository;
+import bgu.adss.fff.dev.domain.models.Product;
+import bgu.adss.fff.dev.exceptions.ProductException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.LinkedList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class ProductServiceImplTests {
@@ -16,12 +25,25 @@ public class ProductServiceImplTests {
     @MockBean
     private ProductRepository productRepository;
 
+    private Product product;
+
     @BeforeEach
     void before() {
+        product = new Product(1, "product", 10.0f, null, new LinkedList<>(), new LinkedList<>(), 5);
     }
 
     @Test
     void testCreate() {
-
+        when(productRepository.save(product)).thenReturn(product);
+        assertEquals(product, productService.createProduct(product));
     }
+
+    @Test
+    void testGet() {
+        when(productRepository.findById(product.getProductID())).thenReturn(Optional.of(product));
+        assertEquals(product, productService.getProduct(product.getProductID()));
+        when(productRepository.findById(-1L)).thenReturn(Optional.empty());
+        assertThrows(ProductException.class, () -> productService.getProduct(-1L));
+    }
+
 }
