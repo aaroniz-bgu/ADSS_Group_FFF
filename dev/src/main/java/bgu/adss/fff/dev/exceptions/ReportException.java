41a432b09b242451f5b9fb787a6bdf29062df28f
ShalevKk <kayatsha@post.bgu.ac.il>
2024-06-11 23:25:08 +0300

    Documented all Exceptions

--- dev/src/main/java/bgu/adss/fff/dev/exceptions/ReportException.java
+++ dev/src/main/java/bgu/adss/fff/dev/exceptions/ReportException.java
@@ -3,6 +3,12 @@ package bgu.adss.fff.dev.exceptions;
 import org.springframework.http.HttpStatus;
 
 public class ReportException extends AppException {
+
+    /**
+     * ReportException constructor
+     * @param message exception message
+     * @param status exception status
+     */
     public ReportException(String message, HttpStatus status) {
         super(message, status);
     }
