5f628ca01c9a44d2037e88d84f841323d86a0b65
aaroniz <aaroni@post.bgu.ac.il>
2024-06-26 13:40:21 +0300

    Created role and custom field for role.

---
+++ dev/src/main/java/bgu/adss/fff/dev/domain/models/RoleField.java
@@ -0,0 +1,63 @@
+package bgu.adss.fff.dev.domain.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+
+@Entity
+public class RoleField {
+
+    @Id
+    private RoleFieldKey id;
+    @Column
+    private String value;
+
+    public RoleField() { }
+
+    protected RoleField(Employee employee, Role role, String field, String value) {
+        this.id = new RoleFieldKey(employee, role, field);
+        this.value = value;
+    }
+
+    public Employee getEmployee() {
+        return id.employee;
+    }
+
+    public Role getRole() {
+        return id.role;
+    }
+
+    public String getField() {
+        return id.field;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Embeddable
+    private static class RoleFieldKey implements Serializable {
+
+        @ManyToOne
+        private Employee employee;
+        @ManyToOne
+        private Role role;
+        private String field;
+
+        protected RoleFieldKey() { }
+
+        protected RoleFieldKey(Employee employee, Role role, String field) {
+            this.employee = employee;
+            this.role = role;
+            this.field = field;
+        }
+    }
+}
